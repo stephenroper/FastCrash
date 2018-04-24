4f217296c2079ce7cd37bb895b7543f2041d9019
Joshua Watson <josh.m.watson@gmail.com>
2018-04-23 19:55:27 -0500

    Scenes 1 and Two;

---
+++ Assets/Scripts/StoryTimerOne.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Assets.Scripts
+{
+    public class StoryTimerOne : MonoBehaviour
+    {
+        public float Timer;
+
+        private void Update()
+        {
+            Timer -= Time.deltaTime;
+            if (Timer <= 0)
+            {
+                SceneManager.LoadScene("Story 2");
+            }
+        }
+    }
+}
